9edba8a70748a92368983af5fdeb49944639ba6c
Ryan Day <soldair@gmail.com>
2013-11-08 21:40:02 -0800

    adding a frame object

--- client/test.js
+++ client/test.js
@@ -1,12 +1,20 @@
 // this is a file for me to test stuff i load it on test.html
 var container = document.getElementById('main');
 
-
+var frameset = require('../lib/frameset');
+var player = require('./player.js')
+var frame = require('../lib/frame')
 var film = require('film')
 
 module.exports = function(video,canvas){
-  return film(video,canvas);
+
+  
+
 }
 
+
+module.exports.frameset = frameset;
+
+module.exports.player = player;
 
 module.exports.film = film;
