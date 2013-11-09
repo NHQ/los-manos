99dcb06849df16d86b8e8bb4931a7c0be8055444
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2013-11-09 08:20:39 -0800

    some ui bs

--- browser.js
+++ browser.js
@@ -1,5 +1,6 @@
 var prefix = require('./prefix.js')().css
 var Film = require('film');
+var spin = require('uxer/spin');
 
 var uuid = require('uuid');
 var framesets = require('./lib/frameset')
@@ -10,32 +11,51 @@ var renderFrame = require('./client/render_frame')
 var frameset = framesets();//(rate) defaults to 5 fps
 
 //composition page
-var showCap = document.getElementById('newFrame')
 var frames = document.getElementById('frameset')
 
 // capture page
-var captureInterface = document.getElementById('capture')
-var closeCap = document.getElementById('closeSourceCap')
-var source = document.getElementById('source')
 var snapShotButton = document.getElementById('snapShot')
 var videoEl = document.getElementById('source')
 var film = document.getElementById('film')
 var mirror = document.getElementById('mirror')
+var shutterSpeed = document.getElementById('shutterSpeed')
 var render = film.getContext('2d');
+var knobs = document.querySelectorAll('.uxer-flatdial')
 
-var h = window.innerHeight
-var camera = Film(videoEl, mirror)
+var params = {
+    shutterSpeed: 100,
+    filmSpeed: 2,
+    r: 0,
+    g: 0,
+    b: 0
+}
 
-showCap.addEventListener('click', function(){   
-    captureInterface.style[prefix+'transform'] = 'translateY('+ h +'px)';
-    captureInterface.style['transform'] = 'translateY('+ h +'px)';
-    source.style.display = 'block'
+Array.prototype.forEach.call(knobs, function(node){
+    spin(node);
+    node.spinDegree = 0;
+    node.addEventListener('spin', function(e){
+        console.log(e)
+    	this.spinDegree += e.detail.delta;
+
+        this.style[prefix + 'transform'] = 'rotateZ('+(e.detail.degree)+'deg)'
+    });
 })
 
-closeCap.addEventListener('click', function(){
-    captureInterface.style[prefix+'transform'] = 'translateY('+ (0 - h) +'px)';
-    captureInterface.style['transform'] = 'translateY('+ (0 - h) +'px)';
-    source.style.display = 'none'
+var h = window.innerHeight
+var camera = Film(videoEl, mirror, film)
+
+shutterSpeed.addEventListener('change', function(e){
+    params.shutterSpeed = parseFloat(this.value)
+    console.log(params)
+})
+
+
+setInterval(function(){
+    camera.expose(params)
+}, 500)
+
+camera.on('expose', function(data){
+    render.putImageData(data, 0, 0)    
 })
 
 snapShotButton.addEventListener('click', function(){
